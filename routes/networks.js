e7b9e90c3059d8c277c4509c76504f454bb37dac
Nick Poulden <nick@poulden.com>
2020-05-01 09:06:05 -0600

    Fix net

--- routes/networks.js
+++ routes/networks.js
@@ -3,7 +3,7 @@ const { Network } = require('../models')
 const { getConfig, setConfig } = require('../utils/encryptedConfig')
 const startListener = require('../listener')
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.post('/networks', authSuperUser, async (req, res) => {
     console.log(req.body)
 
@@ -21,14 +21,16 @@ module.exports = function(app) {
         pinataSecret: req.body.pinataSecret,
         cloudflareEmail: req.body.cloudflareEmail,
         cloudflareApiKey: req.body.cloudflareApiKey,
-        domain: req.body.domain
-      })
+        domain: req.body.domain,
+      }),
     }
 
-    const existing = Network.findOne({ where: { networkId: req.body.netId } })
+    const existing = await Network.findOne({
+      where: { networkId: req.body.netId },
+    })
     if (existing) {
       await Network.update(networkObj, {
-        where: { networkId: networkObj.networkId }
+        where: { networkId: networkObj.networkId },
       })
     } else {
       await Network.create(networkObj)
@@ -41,7 +43,7 @@ module.exports = function(app) {
 
   app.get('/networks/:netId', authSuperUser, async (req, res) => {
     const network = await Network.findOne({
-      where: { networkId: req.params.netId }
+      where: { networkId: req.params.netId },
     })
     if (!network) {
       return res.json({ success: false, reason: 'no-network' })
