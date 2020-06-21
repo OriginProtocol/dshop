c1aa5f9e75bdb6a0ce06cfd4345ce18de4d775df
Shahul Hameed <shahthepro@outlook.com>
2020-06-21 19:27:49 +0530

    Add social links modal

--- shop/src/pages/admin/settings/social-links/SocialLinks.js
+++ shop/src/pages/admin/settings/social-links/SocialLinks.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+import pickBy from 'lodash/pickBy'
+
+import Modal from 'components/Modal'
 
 import Facebook from 'components/icons/Facebook'
 import Twitter from 'components/icons/Twitter'
@@ -7,49 +11,199 @@ import Medium from 'components/icons/Medium'
 import YouTube from 'components/icons/YouTube'
 import PlusIcon from 'components/icons/Plus'
 
+import { formInput, formFeedback } from 'utils/formHelpers'
+
 import Delete from './_Delete'
 
-const SocialLinks = ({ socialLinks }) => {
+const networks = [
+  {
+    value: 'facebook',
+    name: 'Facebook',
+    icon: <Facebook />
+  },
+  {
+    value: 'twitter',
+    name: 'Twitter',
+    icon: <Twitter />
+  },
+  {
+    value: 'instagram',
+    name: 'Instagram',
+    icon: <Instagram />
+  },
+  {
+    value: 'medium',
+    name: 'Medium',
+    icon: <Medium />
+  },
+  {
+    value: 'youtube',
+    name: 'YouTube',
+    icon: <YouTube />
+  }
+]
+
+const validate = (state) => {
+  const newState = {}
+
+  if (!state.network) {
+    newState.networkError = 'Select a network'
+  }
+
+  if (!state.link) {
+    newState.linkError = 'Link is required'
+  } else {
+    try {
+      new URL(state.link)
+    } catch (err) {
+      newState.linkError = 'Not a valid URL'
+    }
+  }
+
+  const valid = Object.keys(newState).every((f) => f.indexOf('Error') < 0)
+
+  return {
+    valid,
+    newState: {
+      ...pickBy(state, (v, k) => !k.endsWith('Error')),
+      ...newState
+    }
+  }
+}
+
+const reducer = (state, newState) => ({ ...state, ...newState })
+
+const SocialLinks = ({ socialLinks, setSocialLinks }) => {
+  const [state, setState] = useReducer(reducer, {
+    showModal: false,
+    shouldClose: false
+  })
+
+  const input = formInput(state, (newState) => setState(newState))
+  const Feedback = formFeedback(state)
+
+  const addLink = async () => {
+    const { valid, newState } = validate(state)
+
+    setState(newState)
+
+    if (!valid) return
+
+    setSocialLinks({
+      [state.network]: state.link
+    })
+
+    setState({
+      shouldClose: true,
+      link: '',
+      network: ''
+    })
+  }
+
   return (
-    <div className="social-links">
-      <div className="title">Social Media Links</div>
-      <div className="links">
-        {!socialLinks.facebook ? null : (
-          <SocialLink icon={<Facebook />} name="Facebook" />
-        )}
-        {!socialLinks.twitter ? null : (
-          <SocialLink icon={<Twitter />} name="Twitter" />
-        )}
-        {!socialLinks.instagram ? null : (
-          <SocialLink icon={<Instagram />} name="Instagram" />
-        )}
-        {!socialLinks.medium ? null : (
-          <SocialLink icon={<Medium />} name="Medium" />
-        )}
-        {!socialLinks.youtube ? null : (
-          <SocialLink icon={<YouTube />} name="YouTube" />
-        )}
+    <>
+      <div className="social-links">
+        <div className="title">Social Media Links</div>
+        <div className="links">
+          {networks.map((network) => {
+            if (!socialLinks[network.value]) return null
+
+            return (
+              <SocialLink
+                icon={network.icon}
+                name={network.name}
+                key={network.value}
+                removeLink={() =>
+                  setSocialLinks({
+                    [network.value]: ''
+                  })
+                }
+              />
+            )
+          })}
+        </div>
+        <div className="mt-3">
+          <button
+            type="button"
+            className="btn btn-outline-primary d-flex align-items-center w-100"
+            onClick={() => setState({ showModal: true })}
+          >
+            <PlusIcon className="mr-2" /> Add Link
+          </button>
+        </div>
       </div>
-      <div className="mt-3">
-        <button
-          type="button"
-          className="btn btn-outline-primary d-flex align-items-center w-100"
+      {!state.showModal ? null : (
+        <Modal
+          shouldClose={state.shouldClose}
+          onClose={() => {
+            setState({
+              shouldClose: false,
+              showModal: false
+            })
+          }}
         >
-          <PlusIcon className="mr-2" /> Add Link
-        </button>
-      </div>
-    </div>
+          <div className="modal-body add-social-link-modal">
+            <h5>Add a Social Media Link</h5>
+            <div className="form-group">
+              <label>Network</label>
+              <select {...input('network')}>
+                <option>Select one</option>
+                {networks.map((network) => (
+                  <option key={network.value} value={network.value}>
+                    {network.name}
+                  </option>
+                ))}
+              </select>
+              {Feedback('network')}
+            </div>
+            <div className="form-group">
+              <label>Link URL</label>
+              <input
+                {...input('link')}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    e.preventDefault()
+                    addLink()
+                  }
+                }}
+              />
+              {Feedback('link')}
+            </div>
+
+            <div className="actions">
+              <button
+                className="btn btn-outline-primary mr-2"
+                type="button"
+                onClick={() =>
+                  setState({
+                    shouldClose: true,
+                    link: '',
+                    network: ''
+                  })
+                }
+              >
+                Cancel
+              </button>
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={addLink}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
   )
 }
 
-const SocialLink = ({ icon, name }) => (
+const SocialLink = ({ icon, name, removeLink }) => (
   <div>
     <div className="icon">{icon}</div>
     {name}
-    <a href="#edit" className="ml-auto" onClick={(e) => e.preventDefault()}>
-      <img src="images/edit-icon.svg" />
-    </a>
-    <Delete className="ml-2" />
+    <Delete className="ml-auto" onConfirm={removeLink} />
   </div>
 )
 
@@ -92,4 +246,18 @@ require('react-styl')(`
         &:not(:last-child)
           border-bottom: 1px solid #cdd7e0
         padding: 0.5rem 0
+  .add-social-link-modal
+    h5
+      margin-top: 1rem
+      text-align: center
+    .actions
+      border-top: 1px solid #cdd7e0
+      padding-top: 1.25rem
+      margin-top: 1.5rem
+      display: flex
+      justify-content: center
+
+      .btn
+        width: 120px
+
 `)
