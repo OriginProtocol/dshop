7f4e2d3d5712369548123f9a611cbdd13a17c119
Franck Chastagnol <franck@originprotocol.com>
2020-06-23 22:10:21 -0700

    Tweak image name

--- backend/scripts/gcp/marketplace/build.sh
+++ backend/scripts/gcp/marketplace/build.sh
@@ -20,7 +20,7 @@ echo "Writing packer.json..."
 BUILD_ID=$(date +%Y%m%d%H%M%S)
 SOURCE_IMAGE="debian-10-buster-v20200618"
 IMAGE_FAMILY="originprotocol-dshop"
-IMAGE_NAME="$IMAGE_FAMILY-vmOS-image-$BUILD_ID"
+IMAGE_NAME="$IMAGE_FAMILY-$SOURCE_IMAGE-image-$BUILD_ID"
 PACKER_ZONE="us-west2-b"
 PACKER_USERNAME="packer"
 TMP_DIR="/tmp/dshop-build-$BUILD_ID"
