e77f6307c25ec6c8b18a866f8702470f5b846325
Franck Chastagnol <franck@originprotocol.com>
2020-07-10 14:03:32 -0700

    Checkpoint

--- backend/scripts/validatePayments.js
+++ backend/scripts/validatePayments.js
@@ -2,28 +2,56 @@ const stripeRaw = require('stripe')
 require('dotenv').config()
 const { Shop, Order } = require('../models')
 const encConf = require('../utils/encryptedConfig')
+const { getLogger } = require('../utils/logger')
+const program = require('commander')
 
-async function validate(dataDir) {
-  const shop = await Shop.findOne({ where: { authToken: dataDir } })
-  if (!shop) {
-    console.log('No shop found')
-    return
+
+const log = getLogger('script')
+
+
+program
+  .option('-i, --shopId <id>', 'Shop Id')
+  .option('-n, --shopName <name>', 'Shop Name')
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+  process.exit(1)
+}
+
+program.parse(process.argv)
+
+async function _getShop() {
+  let shop
+  if (program.shopId) {
+    shop = await Shop.findOne({ where: { id: program.shopId } })
+    if (!shop) {
+      throw new Error(`No shop with id ${program.shopId}`)
+    }
+  } else if (program.shopName) {
+    shop = await Shop.findOne({ where: { name: program.shopName } })
+    if (!shop) {
+      throw new Error(`No shop with name ${program.shopName}`)
+    }
+  } else {
+    throw new Error('Must specify shopId or shopName')
   }
+  return shop
+}
+
+async function validate() {
+  const shop = await _getShop()
 
   const stripeKey = await encConf.get(shop.id, 'stripeBackend')
   if (!stripeKey) {
-    console.log('No stripe key')
-    return
+    throw new Error('No stripe key')
   }
+  log.info(`StripeKey=`)
 
-  const orders = await Order.findAll({
-    where: { shopId: shop.id },
-    limit: 500
-  })
+  const orders = await Order.findAll({ where: { shopId: shop.id } })
   const encryptedHashes = orders
     .map((o) => o.encryptedIpfsHash)
     .filter((i) => i)
-  console.log(`Found ${encryptedHashes.length} orders with encrypted hashes`)
+  log.info(`Found ${encryptedHashes.length} orders with encrypted hashes`)
 
   const stripe = stripeRaw(stripeKey)
 
@@ -36,7 +64,7 @@ async function validate(dataDir) {
         type: 'payment_intent.succeeded',
         starting_after: after
       }
-      console.log(`Fetching events after ${after}`)
+      log.info(`Fetching events after ${after}`)
 
       stripe.events.list(eventArgs, function (err, events) {
         console.log(`Found ${events.data.length} completed Stripe payments`)
@@ -45,11 +73,11 @@ async function validate(dataDir) {
           if (Number(shopId) !== shop.id) {
             /* Ignore */
           } else if (encryptedHashes.indexOf(encryptedData) < 0) {
-            console.log(
-              `No order with hash ${encryptedData}. Created ${item.created}. Amount ${item.data.object.amount}`
+            log.info(
+              `No order with hash ${encryptedData}. Created ${new Date(item.created*1000)}. Amount ${item.data.object.amount}`
             )
           } else {
-            console.log(`Found hash ${encryptedData} OK`)
+            log.info(`Found hash ${encryptedData} OK`)
           }
         })
         after = events.data.length >= 100 ? events.data[99].id : null
@@ -59,10 +87,13 @@ async function validate(dataDir) {
   } while (after)
 }
 
-const dataDir = process.argv[2]
-if (!dataDir) {
-  console.log('Usage: node validatePayments [dataDir]')
-  process.exit()
-}
-
-validate(dataDir)
+validate()
+  .then(() => {
+    log.info('Finished')
+    process.exit()
+  })
+  .catch((err) => {
+    log.error('Failure: ', err)
+    log.error('Exiting')
+    process.exit(-1)
+  })
