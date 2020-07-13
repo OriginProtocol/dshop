eed34f40612bfb34c0db8ad4df87098c73e87f83
Franck Chastagnol <franck@originprotocol.com>
2020-07-13 14:26:33 -0700

    fix migration

--- backend/db/migrations/20200710134411-changeEventTypeToEnum.js
+++ backend/db/migrations/20200710134411-changeEventTypeToEnum.js
@@ -3,25 +3,21 @@
 const { PrintfulWebhookEvents, ExternalServices } = require('../../utils/enums')
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.changeColumn('external_events', 'event_type', {
-        type: Sequelize.ENUM(...Object.values(PrintfulWebhookEvents))
-      }),
-      queryInterface.changeColumn('external_events', 'service', {
-        type: Sequelize.ENUM(...Object.values(ExternalServices))
-      })
-    ])
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('external_events', 'event_type', {
+      type: Sequelize.ENUM(...Object.values(PrintfulWebhookEvents))
+    })
+    await queryInterface.changeColumn('external_events', 'service', {
+      type: Sequelize.ENUM(...Object.values(ExternalServices))
+    })
   },
 
-  down: (queryInterface, Sequelize) => {
-    return Promise.all([
-      queryInterface.changeColumn('external_events', 'event_type', {
-        type: Sequelize.STRING
-      }),
-      queryInterface.changeColumn('external_events', 'service', {
-        type: Sequelize.STRING
-      }),
-    ])
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('external_events', 'event_type', {
+      type: Sequelize.STRING
+    })
+    await queryInterface.changeColumn('external_events', 'service', {
+      type: Sequelize.STRING
+    })
   }
 }
