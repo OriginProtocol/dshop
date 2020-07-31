cb3f6c2d147bb6ce0e7ea6c5339aba8bf2acb19f
Nick Poulden <nick@poulden.com>
2020-07-31 14:29:28 -0600

    Forgot pass

--- shop/src/pages/admin/Login.js
+++ shop/src/pages/admin/Login.js
@@ -10,14 +10,14 @@ import SignUp from 'pages/super-admin/setup/SignUp'
 
 const LoginSignup = () => {
   const [{ admin }] = useStateValue()
-  const [showSignup, setShowSignup] = useState()
+  const [mode, setMode] = useState('login')
 
   const publicSignups = get(admin, 'publicSignups', false)
   if (!publicSignups) {
     return <Login />
   }
 
-  return showSignup ? (
+  return mode === 'sign-up' ? (
     <SetupLayout>
       <div className="mt-3">
         <SignUp url="/register" />
@@ -29,18 +29,20 @@ const LoginSignup = () => {
           href="#"
           onClick={(e) => {
             e.preventDefault()
-            setShowSignup(false)
+            setMode('login')
           }}
           children="Login"
         />
       </div>
     </SetupLayout>
+  ) : mode === 'forgot-pass' ? (
+    <ForgotPass setMode={setMode} />
   ) : (
-    <Login onToggle={() => setShowSignup(true)} />
+    <Login setMode={setMode} />
   )
 }
 
-const Login = ({ onToggle }) => {
+const Login = ({ setMode }) => {
   const history = useHistory()
   const [state, setState] = useState({ email: '', password: '', error: '' })
   const [, dispatch] = useStateValue()
@@ -93,7 +95,7 @@ const Login = ({ onToggle }) => {
           <button type="submit">Login</button>
         </div>
       </form>
-      {!onToggle ? null : (
+      {!setMode ? null : (
         <div className="actions">
           Don&apos;t yet have an account?
           <a
@@ -101,9 +103,74 @@ const Login = ({ onToggle }) => {
             href="#"
             onClick={(e) => {
               e.preventDefault()
-              onToggle()
+              setMode('sign-up')
             }}
             children="Sign Up"
+          />
+          <br />
+          Forgot password?
+          <a
+            className="ml-2"
+            href="#"
+            onClick={(e) => {
+              e.preventDefault()
+              setMode('forgot-pass')
+            }}
+            children="Click here"
+          />
+        </div>
+      )}
+    </SetupLayout>
+  )
+}
+
+const ForgotPass = ({ setMode }) => {
+  const [state, setState] = useState({ email: '', error: '' })
+  const { post } = useBackendApi()
+
+  return (
+    <SetupLayout>
+      <form
+        className="admin login"
+        onSubmit={(e) => {
+          e.preventDefault()
+          setState({ ...state, error: '' })
+          const body = JSON.stringify({ email: state.email })
+          post('/auth/forgot-pass', { body })
+            .then(() => {
+              setState({ ...state, error: '' })
+            })
+            .catch((err) => {
+              console.error('Error signing in', err)
+              setState({ ...state, error: 'Unauthorized' })
+            })
+        }}
+      >
+        <div className="form-group">
+          <label>E-mail</label>
+          <input
+            type="email"
+            className="form-control"
+            value={state.email}
+            autoFocus
+            onChange={(e) => setState({ ...state, email: e.target.value })}
+          />
+        </div>
+        <ErrorText>{state.error}</ErrorText>
+        <div className="form-group mb-0">
+          <button type="submit">Reset password</button>
+        </div>
+      </form>
+      {!setMode ? null : (
+        <div className="actions">
+          <a
+            className="ml-2"
+            href="#"
+            onClick={(e) => {
+              e.preventDefault()
+              setMode('login')
+            }}
+            children="Back to Login"
           />
         </div>
       )}
