7452b2685621e93a5379bab0753f8850373f6541
Franck Chastagnol <franck@originprotocol.com>
2020-08-10 11:06:56 -0700

    Fix typo

--- backend/utils/emails/printfulOrderFailed.js
+++ backend/utils/emails/printfulOrderFailed.js
@@ -13,7 +13,7 @@ const log = getLogger('utils.emailer')
 
 async function sendPrintfulOrderFailedEmail(shopId, orderData, opts, skip) {
   const shop = await Shop.findOne({ where: { id: shopId } })
-  const network = await Network.fineOne({ where: { active: true } })
+  const network = await Network.findOne({ where: { active: true } })
   const { transporter, from, replyTo } = await getShopTransport(shop, network)
   if (!transporter) {
     log.info(`No email transport configured. Skiped sending new order email.`)
