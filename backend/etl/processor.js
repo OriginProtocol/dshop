52696af198e58a8f61aad9fad7ac205d891100b4
Franck Chastagnol <franck@originprotocol.com>
2020-08-11 14:29:54 -0700

    Fetch data from IPFS or disk depending on publish state

--- backend/etl/processor.js
+++ backend/etl/processor.js
@@ -1,3 +1,6 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
 const {
   Discount,
   EtlJob,
@@ -10,66 +13,92 @@ const {
 const { EtlJobStatuses, ShopDeploymentStatuses } = require('../enums')
 const { getConfig } = require('../utils/encryptedConfig')
 const { getShopDataUrl } = require('../utils/shop')
+const { DSHOP_CACHE } = require('../utils/const')
 
 const { getLogger } = require('../utils/logger')
 const log = getLogger('etl.job')
+
+const FETCH_TIMEOUT_MS = 60000 // 60 sec
 
 class EtlJobProcessor {
   constructor(data) {
     this.data = data
   }
 
-  async _fetchJson(url) {
-    const raw = fetch(url)
-    return JSON.parse(raw)
+  async _fetchJson(shop, filename) {
+    if (shop.published) {
+      // Shop is published. Fetch the data from IPFS.
+      const url = `${shop.dataUrl}${filename}`
+      log.debug(`Fetching ${url}`)
+      const response = await fetch(url, { timeout: FETCH_TIMEOUT_MS })
+      return response.json()
+    } else {
+      // Shop is not published yet. Fetch the data from the staging deploy area on disk.
+      const path = `${shop.outputDir}/${filename}`
+      log.debug(`Reading ${path}`)
+      const raw = fs.readFileSync(path)
+      return JSON.parse(raw)
+    }
   }
 
   async _countNumProducts(shop) {
-    const productsUrl = `${shop.dataUrl}/products.json`
-    const products = this._fetchJson(productsUrl)
-    return products.length
+    try {
+      const products = await this._fetchJson(shop, 'products.json')
+      return products.length
+    } catch (e) {
+      log.error(`Shop ${shop.id}: Failed counting products: ${e}`)
+      return null
+    }
   }
 
   async _countNumCollections(shop) {
-    const collectionsUrl = `${shop.dataUrl}/collections.json`
-    const collections = this._fetchJson(collectionsUrl)
-    return collections.length
+    try {
+      const collections = await this._fetchJson(shop, 'collections.json')
+      return collections.length
+    } catch (e) {
+      log.error(`Shop ${shop.id}: Failed counting collections: ${e}`)
+      return null
+    }
+  }
+
+  async _countNumShippings(shop) {
+    try {
+      const shippings = await this._fetchJson(shop, 'shipping.json')
+      return shippings.length
+    } catch (e) {
+      log.error(`Shop ${shop.id}: Failed counting shipping methods: ${e}`)
+      return null
+    }
   }
 
   async _countNumDiscounts(shop) {
-    return await Discount.count({
+    return Discount.count({
       where: { shopId: shop.id, status: 'active' }
     })
   }
 
-  async _countNumShippings(shop) {
-    const shippingUrl = `${shop.dataUrl}/shipping.json`
-    const shippings = this._fetchJson(shippingUrl)
-    return shippings.length
-  }
-
   async _countNumOrders(shop) {
-    return await Order.count({ where: { shopId: shop.id } })
+    return Order.count({ where: { shopId: shop.id } })
   }
 
-  async _crypto(shop) {
+  _crypto(shop) {
     return Boolean(shop.listingId)
   }
 
-  async _stripe(shop) {
+  _stripe(shop) {
     const key = shop.config.stripeBackend
     return key && key.startsWith('sk_live')
   }
 
-  async _paypal(shop) {
+  _paypal(shop) {
     return Boolean(shop.config.paypalClientSecret)
   }
 
-  async _uphold(shop) {
+  _uphold(shop) {
     return Boolean(shop.config.upholdSecret)
   }
 
-  async _manualPayment(shop) {
+  _manualPayment(shop) {
     const payments = shop.config.offlinePaymentMethods
     if (payments && Array.isArray(payments)) {
       const count = payments.filter((p) => p.disabled).length
@@ -78,33 +107,33 @@ class EtlJobProcessor {
     return false
   }
 
-  async _printful(shop) {
+  _printful(shop) {
     return Boolean(shop.config.printful)
   }
 
-  async _sendgrid(shop) {
+  _sendgrid(shop) {
     return Boolean(shop.config.sendgridApiKey)
   }
 
-  async _aws(shop) {
+  _aws(shop) {
     return Boolean(shop.config.awsAccessSecret)
   }
 
-  async _mailgun(shop) {
+  _mailgun(shop) {
     return Boolean(shop.config.mailgunSmtpPassword)
   }
 
   async _published(shop) {
-    const published = ShopDeployment.findOne({
+    const published = await ShopDeployment.findOne({
       where: { shopId: shop.id, status: ShopDeploymentStatuses.Success }
     })
     return Boolean(published)
   }
 
-  async _customDomain(shop) {
-    // TODO(franck): figure out how to extract that information.
-    log.debug(`${shop.id}: custom domain extraction not implemented yet.`)
-    return false
+  _customDomain(shop) {
+    // TODO(franck): figure out how to extract this information.
+    log.warn(`Shop ${shop.id}: custom domain extraction not implemented yet.`)
+    return null
   }
 
   /**
@@ -120,25 +149,33 @@ class EtlJobProcessor {
     data.numDiscounts = await this._countNumDiscounts(shop)
     data.numDiscounts = await this._countNumShippings(shop)
     data.numOrders = await this._countNumOrders(shop)
-    data.crypto = await this._crypto(shop)
-    data.stripe = await this._stripe(shop)
-    data.paypal = await this._paypal(shop)
-    data.uphold = await this._uphold(shop)
-    data.manualPayment = await this._manualPayment(shop)
-    data.printful = await this._printful(shop)
-    data.sendgrid = await this._sendgrid(shop)
-    data.aws = await this._aws(shop)
-    data.mailgun = await this._mailgun(shop)
-    data.published = await this._published(shop)
-    data.customDomain = await this._customDomain(shop)
+    data.crypto = this._crypto(shop)
+    data.stripe = this._stripe(shop)
+    data.paypal = this._paypal(shop)
+    data.uphold = this._uphold(shop)
+    data.manualPayment = this._manualPayment(shop)
+    data.printful = this._printful(shop)
+    data.sendgrid = this._sendgrid(shop)
+    data.aws = this._aws(shop)
+    data.mailgun = this._mailgun(shop)
+    data.published = shop.published
+    data.customDomain = this._customDomain(shop)
+    return data
   }
 
-  _decorateShop(shop, netConfig) {
+  async _decorateShop(shop, netConfig) {
     // Replace the shop's encrypted config with its decrypted version.
     shop.config = getConfig(shop.config)
 
+    // Determine if the shop was published. This is used when extracting data
+    // to decide on what source to use: IPFS vs deploy staging area on disk.
+    shop.published = await this._published(shop)
+
     // Get the shop's data URL.
     shop.dataUrl = getShopDataUrl(shop, netConfig)
+
+    // Get the shop's deploy dir.
+    shop.outputDir = `${DSHOP_CACHE}/${shop.auth_token}`
   }
 
   /**
@@ -154,15 +191,20 @@ class EtlJobProcessor {
 
     const shops = await Shop.findAll({ order: [['id', 'asc']] })
     for (const shop of shops) {
-      log.info(`Processing shop ${shop.id}`)
+      try {
+        log.info(`Processing shop ${shop.id}`)
 
-      await this._decorateShop(shop, netConfig)
+        await this._decorateShop(shop, netConfig)
 
-      // Extract data from the shop and insert it as a new EtlShop row.
-      const data = this.processShop(shop)
-      await EtlShop.create({ jobId: job.id, ...data })
+        // Extract data from the shop and insert it as a new EtlShop row.
+        const data = this.processShop(shop)
+        await EtlShop.create({ jobId: job.id, ...data })
+      } catch (e) {
+        // Log the error but continue processing other shops.
+        log.error(`Shop ${shop.id}: Failed extracting ETL data: ${e}`)
+      }
     }
-
+    // Mark the job as successfully completed.
     await job.update({ status: EtlJobStatuses.Success })
   }
 }
