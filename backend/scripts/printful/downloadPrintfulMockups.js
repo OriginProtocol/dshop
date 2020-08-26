11fe40105bbcefc19f3b134b5f046edecc3c310c
Nick Poulden <nick@poulden.com>
2020-08-26 16:07:31 -0600

    Option to force new Printful images (#507)

--- backend/scripts/printful/downloadPrintfulMockups.js
+++ backend/scripts/printful/downloadPrintfulMockups.js
@@ -5,10 +5,11 @@ const { getLogger } = require('../../utils/logger')
 
 const log = getLogger('scripts.printful.downloadPrintfulMockups')
 
-async function downloadPrintfulMockups({ OutputDir, png }) {
+async function downloadPrintfulMockups({ OutputDir, png, force }) {
   const filesRaw = fs.readFileSync(`${OutputDir}/printful-images.json`)
   const files = JSON.parse(filesRaw).filter((f) => !f.skip)
-  log.info(`Downloading ${files.length} mockups...`)
+  const forceMsg = force ? ' (force new images)' : ''
+  log.info(`Downloading ${files.length} mockups${forceMsg}...`)
 
   for (const file of files) {
     const prefix = `${OutputDir}/data/${file.id}/orig`
@@ -16,17 +17,22 @@ async function downloadPrintfulMockups({ OutputDir, png }) {
     const filename = `${prefix}/${file.file}`
     const filenameOut = png ? filename : filename.replace('.png', '.jpg')
     // log.debug(filenameOut)
-    if (!fs.existsSync(filenameOut)) {
+    if (!fs.existsSync(filenameOut) || force) {
       await new Promise((resolve) => {
         const f = fs.createWriteStream(filename).on('finish', resolve)
         https.get(file.url, (response) => response.pipe(f))
       })
       let resizedFile
       if (png) {
-        resizedFile = await sharp(filename).toBuffer()
+        resizedFile = await sharp(filename)
+          .flatten({ background: { r: 255, g: 255, b: 255 } })
+          .toBuffer()
         fs.writeFileSync(filenameOut, resizedFile)
       } else {
-        resizedFile = await sharp(filename).jpeg().toBuffer()
+        resizedFile = await sharp(filename)
+          .flatten({ background: { r: 255, g: 255, b: 255 } })
+          .jpeg()
+          .toBuffer()
         fs.writeFileSync(filenameOut, resizedFile)
         fs.unlinkSync(filename)
       }
