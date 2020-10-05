125a36ee5c4fda1925903591d2e03a568246a8b4
Franck Chastagnol <franck@originprotocol.com>
2020-10-05 13:44:30 -0700

    DB schema

---
+++ backend/models/admin_log.js
@@ -0,0 +1,14 @@
+const { AdminLogActions } = require('../enums')
+
+module.exports = (sequelize, DataTypes) => {
+  const isPostgres = sequelize.options.dialect === 'postgres'
+
+  const AdminLog = sequelize.define('AdminLog', {
+    action: DataTypes.ENUM(AdminLogActions), // The type of action performed.
+    shopId: DataTypes.INTEGER, // The shop the action was applied to.
+    sellerId: DataTypes.INTEGER, // The user that initiated the action.
+    data: isPostgres ? DataTypes.JSONB : DataTypes.JSON // Data related to the action.
+  })
+
+  return AdminLog
+}
