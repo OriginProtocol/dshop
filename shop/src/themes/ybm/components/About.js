ba12bee554ae1d9381f734afcc9af4dd05c8089b
Shahul Hameed <shahthepro@outlook.com>
2020-10-22 12:24:09 +0530

    ybm theme changes

--- shop/src/themes/ybm/components/About.js
+++ shop/src/themes/ybm/components/About.js
@@ -15,24 +15,25 @@ const About = () => {
   const aboutDescription = get(themeVars, 'about.description')
   const header = get(themeVars, 'about.headerImage.0', {})
   const primaryImage = get(themeVars, 'about.primaryImage.0', {})
-  const aboutImages = useMemo(() => {
-    const allImages = get(themeVars, 'about.aboutImages', [])
-    const col1Images = []
-    const col2Images = []
+  const aboutImages = get(themeVars, 'about.aboutImages', [])
+  // useMemo(() => {
+  //   const allImages = get(themeVars, 'about.aboutImages', [])
+  //   const col1Images = []
+  //   const col2Images = []
 
-    allImages.map((img, index) => {
-      if (index % 2 === 0) {
-        col1Images.push(img)
-      } else {
-        col2Images.push(img)
-      }
-    })
+  //   allImages.map((img, index) => {
+  //     if (index % 2 === 0) {
+  //       col1Images.push(img)
+  //     } else {
+  //       col2Images.push(img)
+  //     }
+  //   })
 
-    return {
-      col1Images,
-      col2Images
-    }
-  }, [themeVars])
+  //   return {
+  //     col1Images,
+  //     col2Images
+  //   }
+  // }, [themeVars])
 
   return (
     <>
@@ -47,17 +48,17 @@ const About = () => {
         </div>
       </Header>
       <div className="sm:container sm:my-16 flex flex-col sm:flex-row">
-        <div className="flex-1 order-2 sm:order-1">
-          <div
-            style={{
-              paddingTop: primaryImage.height,
-              backgroundSize: primaryImage.backgroundSize,
-              backgroundImage: `url(${config.dataSrc}${primaryImage.url})`,
-              backgroundPosition: primaryImage.backgroundPosition
-            }}
-            className="bg-no-repeat bg-center"
-          />
-        </div>
+        {!primaryImage.url ? null : (
+          <div className="flex-1 order-2 sm:order-1 bg-cover bg-no-repeat bg-center">
+            <div
+              style={{
+                paddingTop: '128%',
+                backgroundImage: `url(${config.dataSrc}${primaryImage.url})`
+              }}
+              className="bg-no-repeat bg-center"
+            />
+          </div>
+        )}
         <div style={{ flex: '1' }} className=" order-1 sm:order-2">
           {!aboutText ? null : (
             <div
@@ -74,29 +75,31 @@ const About = () => {
         <div className="sm:container">
           <div className="flex gap-4 sm:gap-10">
             <div className="flex-1 flex flex-col gap-4 sm:gap-10">
-              {aboutImages.col1Images.map((img, idx) => (
+              {aboutImages.slice(0, 2).map((img, idx) => (
                 <div
                   key={idx}
-                  className="bg-no-repeat"
+                  className="bg-no-repeat bg-center bg-cover"
                   style={{
-                    paddingTop: img.height,
-                    backgroundSize: img.backgroundSize,
-                    backgroundPosition: img.backgroundPosition,
-                    backgroundImage: `url(${config.dataSrc}${img.url})`
+                    paddingTop: idx === 0 ? '71%' : '128%',
+                    backgroundImage:
+                      !img || !img.url
+                        ? null
+                        : `url(${config.dataSrc}${img.url})`
                   }}
                 />
               ))}
             </div>
             <div className="flex-1 flex flex-col gap-4 sm:gap-10">
-              {aboutImages.col2Images.map((img, idx) => (
+              {aboutImages.slice(2, 4).map((img, idx) => (
                 <div
                   key={idx}
-                  className="bg-no-repeat"
+                  className="bg-no-repeat bg-center bg-cover"
                   style={{
-                    paddingTop: img.height,
-                    backgroundSize: img.backgroundSize,
-                    backgroundPosition: img.backgroundPosition,
-                    backgroundImage: `url(${config.dataSrc}${img.url})`
+                    paddingTop: idx === 0 ? '128%' : '71%',
+                    backgroundImage:
+                      !img || !img.url
+                        ? null
+                        : `url(${config.dataSrc}${img.url})`
                   }}
                 />
               ))}
