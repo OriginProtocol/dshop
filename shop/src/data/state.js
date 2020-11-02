9208fc1ca0b84c40e52e7081e9bfa22013e64ef7
Franck Chastagnol <franck@originprotocol.com>
2020-11-02 14:45:58 -0800

    prettier

--- shop/src/data/state.js
+++ shop/src/data/state.js
@@ -363,12 +363,14 @@ const reducer = (state, action) => {
   newState.cart.currency = get(newState, 'config.currency', 'USD')
   newState.cart.discount = discount
   newState.cart.totalTaxes = Math.ceil(taxRate * newState.cart.subTotal)
-  newState.cart.total = Math.max(0,
+  newState.cart.total = Math.max(
+    0,
     newState.cart.subTotal +
-    shipping -
-    discount +
-    donation +
-    newState.cart.totalTaxes)
+      shipping -
+      discount +
+      donation +
+      newState.cart.totalTaxes
+  )
 
   const activeShop = get(newState, 'config.activeShop')
   if (activeShop) {
