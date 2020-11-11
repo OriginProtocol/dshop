4eca5cf44ff15d18c21e647e1d7938b12fb158ff
Zakir Hussain <jakirdeveloper@gmail.com>
2020-11-12 00:17:06 +0530

    Add test case

--- backend/test/discount.test.js
+++ backend/test/discount.test.js
@@ -309,4 +309,71 @@ describe('Discounts', () => {
       expect(order.data).to.eql(expectedData)
     }
   })
+
+  it('Should exclude shipping fee', async () => {
+    const withExcludeShipping = {
+      ...percentageDiscount.get({ plain: true }),
+      excludeShipping: true
+    }
+    const withOutExcludeShipping = {
+      ...percentageDiscount.get({ plain: true }),
+      excludeShipping: false
+    }
+
+    const discounts = [
+      pick(withExcludeShipping, [
+        'code',
+        'discountType',
+        'value',
+        'data',
+        'excludeShipping'
+      ]),
+      pick(withOutExcludeShipping, [
+        'code',
+        'discountType',
+        'value',
+        'data',
+        'excludeShipping'
+      ])
+    ]
+    for (const discount of discounts) {
+      const { ipfsHash, data } = await createTestEncryptedOfferData(
+        network,
+        shop,
+        key,
+        { discount }
+      )
+
+      // Create a mock Bull job object.
+      const jobData = {
+        shopId: shop.id,
+        amount: data.total,
+        encryptedDataIpfsHash: ipfsHash,
+        paymentCode: 'testPaymentCode' + Date.now(),
+        paymentType: OrderPaymentTypes.CryptoCurrency
+      }
+      job = new MockBullJob(jobData)
+
+      // Have the queue process the order.
+      const order = await processor(job)
+
+      expect(order).to.be.an('object')
+      expect(order.fqId).to.be.a('string')
+      expect(order.shortId).to.be.a('string')
+      expect(order.networkId).to.equal(999)
+      expect(order.shopId).to.equal(shop.id)
+      expect(order.paymentStatus).to.equal(OrderPaymentStatuses.Paid)
+      expect(order.offerId).to.be.undefined
+      expect(order.offerStatus).to.be.undefined
+      expect(order.createdBlock).to.be.undefined
+      expect(order.updatedBlock).to.be.undefined
+      expect(order.ipfsHash).to.be.a('string')
+      expect(order.encryptedIpfsHash).to.equal(ipfsHash)
+      expect(order.paymentCode).to.equal(jobData.paymentCode)
+      expect(order.paymentType).to.equal(jobData.paymentType)
+      expect(order.total).to.equal(data.total)
+      expect(order.currency).to.equal(data.currency)
+      expect(order.data).to.eql(data)
+    }
+  })
 })
