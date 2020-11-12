ded65c3b1c74fb095756e9c2b1dccf7839b44150
Franck Chastagnol <franck@originprotocol.com>
2020-11-11 21:11:46 -0800

    fix

--- backend/db/migrations/20201111223056-addColumnActiveToEtlShop.js
+++ backend/db/migrations/20201111223056-addColumnActiveToEtlShop.js
@@ -3,12 +3,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async () => {
-      await queryInterface.addColumn('active', 'etl_shops', { type: Sequelize.BOOLEAN, })
+      await queryInterface.addColumn('etl_shops', 'active', { type: Sequelize.BOOLEAN, })
     })
   },
   down: (queryInterface) => {
     return queryInterface.sequelize.transaction(async () => {
-      await queryInterface.removeColumn('active', 'etl_shops')
+      await queryInterface.removeColumn('etl_shops', 'active')
     })
   }
 }
