5103a7728b16a63621d0e3b1abf619e69a651a34
Luiz Lopes <luiz@wickeddeveloper.com>
2020-11-12 21:00:53 -0700

    feat: auto-select first payment option
    Add functionality to select the first payment option whenever the payment component mounts and the end-user has not made a selection on their own.

--- shop/src/pages/checkout/ChoosePayment.js
+++ shop/src/pages/checkout/ChoosePayment.js
@@ -116,10 +116,10 @@ const ChoosePayment = () => {
   )
 
   useEffect(() => {
-    if (paymentMethods.length === 1) {
+    if (paymentMethods.length === 1 || paymentMethod == undefined) {
       dispatch({ type: 'updatePaymentMethod', method: paymentMethods[0] })
     }
-  }, [paymentMethods.length])
+  }, [paymentMethods.length, paymentMethod])
 
   const isOfflinePayment = !!get(cart, 'paymentMethod.instructions', false)
 
