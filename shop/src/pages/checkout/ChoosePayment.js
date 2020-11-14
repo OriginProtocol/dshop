375638b04db9c8ca9acef262de0e215e4918fed4
Luiz Lopes <luiz@wickeddeveloper.com>
2020-11-13 17:31:20 -0700

    refactor: ensure paymentMethods is not empty
    Updates hook that sets the first payment option as the selected payment option by first checking if there are payment methods available.
    
    🔗 https://github.com/OriginProtocol/dshop/pull/775#discussion_r522650271

--- shop/src/pages/checkout/ChoosePayment.js
+++ shop/src/pages/checkout/ChoosePayment.js
@@ -116,7 +116,9 @@ const ChoosePayment = () => {
   )
 
   useEffect(() => {
-    if (paymentMethods.length === 1 || paymentMethod === undefined) {
+    const shouldSelectFirstOptions =
+      paymentMethods.length >= 1 && paymentMethod === undefined
+    if (shouldSelectFirstOptions) {
       dispatch({ type: 'updatePaymentMethod', method: paymentMethods[0] })
     }
   }, [paymentMethods.length, paymentMethod])
