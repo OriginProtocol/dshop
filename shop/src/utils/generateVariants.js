f9200ac4ca52bf3601feec8c12c466e232fbb20b
Rajath <>
2020-12-15 17:03:05 +0530

    Proposing new logic for generating variants; added handling for empty array inputs for 'getAllCombinations'

--- shop/src/utils/generateVariants.js
+++ shop/src/utils/generateVariants.js
@@ -1,25 +1,42 @@
-import flatMap from 'lodash/flatMap'
+const combinationsOfElementAndArray = (element, arr) => {
+  let accumulator = []
+  if (arr.length === 0) {
+    accumulator = [...accumulator, [element, arr[0]]]
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      accumulator = [...accumulator, [element, ...arr[i]]]
+    } else {
+      accumulator = [...accumulator, [element, arr[i]]]
+    }
+  }
+  return accumulator
+}
+
+const combinationsOfTwoArrays = (array1, array2) => {
+  let combinations = []
+  if (array1.length === 0) {
+    combinations = [
+      ...combinations,
+      ...combinationsOfElementAndArray(array1[0], array2)
+    ]
+  }
+  for (let j = 0; j < array1.length; j++) {
+    combinations = [
+      ...combinations,
+      ...combinationsOfElementAndArray(array1[j], array2)
+    ]
+  }
+  return combinations
+}
 
 export const getAllCombinations = (array1, ...arrays) => {
-  const combinationsOf2Array = (a, b) =>
-    a.reduce(
-      (combinations, v1) => [
-        ...combinations,
-        ...b.map((v2) => flatMap([v1, v2]))
-      ],
-      []
-    )
-
-  if (arrays.length === 1) {
-    return combinationsOf2Array(array1, arrays[0])
-  } else if (arrays.length >= 2) {
-    return getAllCombinations(
+  if (arrays.length === 1) return combinationsOfTwoArrays(array1, arrays[0])
+  else if (arrays.length >= 2)
+    return combinationsOfTwoArrays(
       array1,
-      getAllCombinations(arrays[0], ...arrays.slice(1))
+      getAllCombinations(arrays[0], ...arrays.splice(1))
     )
-  }
-
-  return array1.map((x) => [x])
 }
 
 export const generateVariants = (product) => {
@@ -29,7 +46,7 @@ export const generateVariants = (product) => {
   if (!options.length || !availableOptions.length) return []
 
   // Map existing variants for easy access and carrying over values
-  const existingVairants = (variants || []).reduce((obj, variant) => {
+  const existingVariants = (variants || []).reduce((obj, variant) => {
     return {
       ...obj,
       [variant.options.join('|||')]: variant
@@ -49,7 +66,7 @@ export const generateVariants = (product) => {
         available: true,
 
         // Default to previous values if it exists
-        ...existingVairants[optionCombo.join('|||')],
+        ...existingVariants[optionCombo.join('|||')],
 
         // Set {option1, option2, ...} values
         ...optionCombo.reduce(
