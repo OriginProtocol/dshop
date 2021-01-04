94f9a90250994fa0145c7d621fcd8824ae2bdc1d
Shahul Hameed <shahthepro@outlook.com>
2021-01-04 21:41:03 +0530

    Bug fixes (#863)

--- backend/scripts/printful/getProduct.js
+++ backend/scripts/printful/getProduct.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const { get } = require('./_api')
+const _get = require('lodash/get')
 
 const getSizeGuide = require('./getSizeGuide')
 
@@ -8,10 +9,10 @@ async function getProduct({ apiAuth, OutputDir, id }) {
   const json = await get(`/sync/products/${id}`, { auth: apiAuth })
   fs.writeFileSync(syncPath, JSON.stringify(json.result, null, 2))
 
-  const productId = json.result.sync_variants[0].product.product_id
+  const productId = _get(json, 'result.sync_variants[0].product.product_id')
   const productPath = `${OutputDir}/data-printful/product-${productId}.json`
 
-  if (!fs.existsSync(productPath)) {
+  if (productId && !fs.existsSync(productPath)) {
     const productJson = await get(`/products/${productId}`, { auth: apiAuth })
     const result = productJson.result
     const sizeGuide = await getSizeGuide({ OutputDir, productId })
