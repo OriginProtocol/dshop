4d1b52bcca949c792d9a56c3f2f01ca1640ac118
Franck <franck@originprotocol.com>
2021-02-05 16:04:00 -0800

    Skip discord notifications for autossl failures (#902)
    * merge
    
    * prettier
    
    * tweak
    
    * code cleanup

--- backend/queues/index.js
+++ backend/queues/index.js
@@ -1,9 +1,44 @@
+const { Sentry } = require('../sentry')
 const discordWebhook = require('../utils/discordWebhook')
 const queues = require('./queues')
 const get = require('lodash/get')
 
+// Queues for which we log errors in Sentry.
+const sentryWhitelistedQueues = ['events', 'makeOffer', 'printfulSync', 'tx']
+
+// Queues and shops for which we don't post failures in discord.
+const discordBlacklistedQueues = ['autossl']
+const discordBlacklistedShopIds = [1] // Gitcoin (shopId=1) is deleted.
+
+/**
+ * Queue failure callback function.
+ */
+function failureCallback(job, error) {
+  const queueName = job.queue.name
+  const shopId = get(job, 'data.shopId', -1)
+
+  if (sentryWhitelistedQueues.includes(queueName)) {
+    Sentry.captureException(error)
+  }
+
+  if (
+    discordBlacklistedQueues.includes(queueName) ||
+    discordBlacklistedShopIds.includes(Number(shopId))
+  ) {
+    discordWebhook.postQueueError({
+      queueName,
+      errorMessage: error.message,
+      jobId: job.id,
+      shopId,
+      attempts: job.attemptsMade,
+      stackTrace: error.stack
+    })
+  }
+}
+
 /**
  * Attaches all backend queue processing functions to their respective queues.
+ * Registers up a failure callback.
  */
 function runProcessors() {
   require('./makeOfferProcessor').attachToQueue()
@@ -15,17 +50,7 @@ function runProcessors() {
   require('./deploymentProcessor').attachToQueue()
   require('./dnsStatusProcessor').attachToQueue()
 
-  const failureCallback = (job, error) => {
-    discordWebhook.postQueueError({
-      queueName: job.queue.name,
-      errorMessage: error.message,
-      jobId: job.id,
-      shopId: get(job, 'data.shopId', ''),
-      attempts: job.attemptsMade,
-      stackTrace: error.stack
-    })
-  }
-
+  // Registers the failure callback
   for (const queueName in queues) {
     queues[queueName].on('failed', failureCallback)
   }
