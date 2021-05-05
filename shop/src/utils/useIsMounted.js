66a80d9696e27df72fbc92a851c8bde08d808add
baaae <reecehudson10@gmail.com>
2021-05-05 12:11:14 -0600

    add comments to useIsMounted

--- shop/src/utils/useIsMounted.js
+++ shop/src/utils/useIsMounted.js
@@ -1,9 +1,15 @@
 import { useEffect, useRef } from 'react'
 
 export default function useIsMounted() {
+  // use something similar to the plain mutable class member
   const isMounted = useRef(true)
 
-  useEffect(() => () => (isMounted.current = false), [])
+  // cleanup fires once when component unmounts
+  useEffect(() => {
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
 
   return isMounted
 }
