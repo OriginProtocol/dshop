89be6bf3a75b01d298bc2f6a7f69aab5bb90f436
Rajath <rajathsbhat@gmail.com>
2021-06-30 17:05:40 +0530

    skeleton code for backend manipulation of store policies

---
+++ backend/routes/policies.js
@@ -0,0 +1,10 @@
+const { Policies } = require('../models')
+
+module.exports = function (router){
+	router.post('', , async (req, res) => {
+		//https://sequelize.org/master/manual/model-querying-basics.html
+	})
+	router.get()
+	router.put()
+	router.delete()
+}
