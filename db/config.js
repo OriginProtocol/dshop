02e30fe64ffec3c5d1b95880ec97200cde548f45
Franck Chastagnol <franck@originprotocol.com>
2021-07-28 16:30:53 -0700

    fix config.js

--- db/config.js
+++ db/config.js
@@ -6,5 +6,19 @@ const url = process.env.DATABASE_URL || sqlite
 module.exports = {
   development: { url, logging: false, define: { underscored: true } },
   test: { url, logging: false, define: { underscored: true } },
-  production: { url, logging: false, define: { underscored: true }, dialectOptions: { ssl: { require: true, rejectUnauthorized: false } } }
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    define: {
+      underscored: true
+    },
+    // Below is required for Heroku Postgres to work.
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    // Disable logging of SQL statements.
+    logging: false
+  }
 }
