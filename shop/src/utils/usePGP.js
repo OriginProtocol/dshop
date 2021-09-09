ca721a1a3d5457df7cba17d70a566dce0bbbf416
Rajath <rajathsbhat@gmail.com>
2021-09-09 15:10:20 +0530

    Reverted previous change to usePGP module

--- shop/src/utils/usePGP.js
+++ shop/src/utils/usePGP.js
@@ -11,11 +11,11 @@ function usePGP() {
     setLoading(true)
 
     const pgpEl = document.createElement('script')
-    pgpEl.src = '/dist/openpgp.min.js'
+    pgpEl.src = 'dist/openpgp.min.js'
     pgpEl.onload = async () => {
       openpgp.config.show_comment = false
       openpgp.config.show_version = false
-      await openpgp.initWorker({ path: '/dist/openpgp.worker.min.js' })
+      await openpgp.initWorker({ path: 'dist/openpgp.worker.min.js' })
       setLoaded(true)
     }
     document.head.appendChild(pgpEl)
